76f8db73db27a5b7c7e1bb6a18e4f47571986964
Pablo Giraldo <j.giraldo@chaione.com>
2024-10-03 23:55:32 -0500

    feat(docker): Configuring Dockerfile and docker-compose to run the app

--- src/application/singleton.ts
+++ src/application/singleton.ts
@@ -1,25 +1,11 @@
 import { PgConfiguration } from "@/application/config/pg-instance";
-import { PgConfiguration } from "@/application/config/pg-instance";
-import { PgConfiguration } from "@/application/config/pg-instance";
 
 /**
-   * This array has all the singleton instances of the application
-   */
+ * This array has all the singleton instances of the application
+ */
 export const singletonInitializers: Array<() => Promise<void>> = [
-    async () =>
-    {
-        const pgConfig = PgConfiguration.getInstance();
-        await pgConfig.managerConnectionPg();
-    },
-    async () =>
-    {
-        const pgConfig = PgConfiguration.getInstance();
-        await pgConfig.managerConnectionPg();
-    },
-    async () =>
-    {
-        const pgConfig = PgConfiguration.getInstance();
-        await pgConfig.managerConnectionPg();
-    },
-
+  async () => {
+    const pgConfig = PgConfiguration.getInstance();
+    await pgConfig.managerConnectionPg();
+  },
 ];
