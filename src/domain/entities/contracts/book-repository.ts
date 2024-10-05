215056df3f5103e589d2ca8852ee9f6d222370d6
Pablo Giraldo <j.giraldo@chaione.com>
2024-10-05 04:46:01 -0500

    feat(controller): Adding book controller

--- src/domain/entities/contracts/book-repository.ts
+++ src/domain/entities/contracts/book-repository.ts
@@ -1,6 +1,7 @@
 export const BOOK_REPOSITORY = 'BOOK_REPOSITORY';
 
 export interface IBookRepository {
+  getBooks(): Promise<any[]>;
   getBookById(bookId: number): Promise<any>;
   updateBookStatus(bookId: number, status: 'available' | 'borrowed'): Promise<void>;
 }
--- src/infrastructure/entry-points/api/books-controller.ts
+++ src/infrastructure/entry-points/api/books-controller.ts
@@ -1,12 +1,15 @@
-import {Mapping, Get} from "@tsclean/core";
+import { Mapping, Get, Adapter } from "@tsclean/core";
+import { IBookRepository, BOOK_REPOSITORY } from "@/domain/entities/contracts/book-repository";
+import { BookEntity } from "@/domain/entities/book";
 
 @Mapping('api/v1/books')
 export class BooksController {
-    constructor() {
-    }
+    constructor(
+        @Adapter(BOOK_REPOSITORY) private readonly bookRepository: IBookRepository
+    ) {}
 
     @Get()
-    async getBooks(): Promise<any> {
-        return 'Listing all books'
+    async getBooks(): Promise<BookEntity[]> {
+        return this.bookRepository.getBooks();
     }
 }
