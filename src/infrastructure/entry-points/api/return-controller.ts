66727d1beeb59acaf3357e94263a6157228bdb59
Pablo Giraldo <juan@maxretail.com>
2024-10-07 00:17:36 -0500

    feat(return): Configuring return endpoint

--- src/infrastructure/entry-points/api/return-controller.ts
+++ src/infrastructure/entry-points/api/return-controller.ts
@@ -1,12 +1,19 @@
-import {Mapping, Post, Body} from "@tsclean/core";
+import { Mapping, Post, Body, Adapter } from "@tsclean/core";
+import { IReturnBookService, RETURN_BOOK_SERVICES } from "@/domain/use-cases/return-book-service";
 
 @Mapping('api/v1/return')
 export class ReturnController {
-    constructor() {
-    }
+    constructor(
+        @Adapter(RETURN_BOOK_SERVICES) private readonly returnBookService: IReturnBookService
+    ) {}
 
     @Post()
-    async returnBook(@Body() returnRequest: any): Promise<any> {
-        return 'Book returned successfully';
+    async returnBook(@Body() returnRequest: { userId: number, bookId: number }): Promise<any> {
+        try {
+            await this.returnBookService.returnBook(returnRequest.userId, returnRequest.bookId);
+            return { message: 'Book returned successfully' };
+        } catch (error) {
+            return { message: error.message };
+        }
     }
 }
