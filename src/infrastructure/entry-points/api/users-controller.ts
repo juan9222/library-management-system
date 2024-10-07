3a59032ad384966439fd64f8bcc4798562699cdf
Pablo Giraldo <juan@maxretail.com>
2024-10-07 00:24:51 -0500

    feat(return): Configuring users endpoint

--- src/infrastructure/entry-points/api/users-controller.ts
+++ src/infrastructure/entry-points/api/users-controller.ts
@@ -1,17 +1,19 @@
-import {Mapping, Get, Param} from "@tsclean/core";
+import { Mapping, Get, Param, Adapter } from "@tsclean/core";
+import { ITransactionHistoryService, TRANSACTION_HISTORY_SERVICES } from "@/domain/use-cases/transaction-history-service";
 
 @Mapping('api/v1/users/')
 export class UsersController {
-    constructor() {
-    }
+    constructor(
+        @Adapter(TRANSACTION_HISTORY_SERVICES) private readonly transactionHistoryService: ITransactionHistoryService
+    ) {}
 
     @Get(':id/borrowed-books')
     async getBorrowedBooks(@Param("id") userId: string): Promise<any> {
-        return `List of borrowed books for user ${userId}`; 
+        return this.transactionHistoryService.getBorrowedBooks(Number(userId));
     }
 
     @Get(':id/history')
     async getUserHistory(@Param("id") userId: string): Promise<any> {
-        return `Borrowing history for user ${userId}`; 
+        return this.transactionHistoryService.getUserHistory(Number(userId));
     }
 }
